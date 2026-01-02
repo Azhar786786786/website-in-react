38a143b2e2c210a99a50613550a7120cc2591448
Azhar786786786 <talkazhar786@hotmail.com>
2026-01-02 11:40:38 +0530

    added links in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-      Footer Content
+
+      <h4>
+        Footer Content and links are here <br />
+        <a class="social-link" href="https://github.com/Azhar786786786">Github</a><span>&nbsp;&nbsp;</span>
+        <a class="social-link" href="https://www.linkedin.com/in/azhar-hussain-98775411a/">LinkedIn</a><span>&nbsp;&nbsp;</span>
+        <a class="social-link" href="https://azharhussain.netlify.app/">MyProfile</a>
+      </h4>
     </footer>
   )
 }
