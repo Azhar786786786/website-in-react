22e7dc66e58c6e5f3a792d0eb6e8a1197758ebf9
Azhar786786786 <talkazhar786@hotmail.com>
2026-01-03 12:51:58 +0530

    added datetime in header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import UseStateComponent from './Hooks/UseStateComponent';
 import UseEffectComponent from './Hooks/UseEffectComponent';
 
 function Header() {
+
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer); // cleanup
+  }, []);
+
   return (
     <>
       <header>
-        Header Content
+        Header Content : <span style={{color:"yellow"}}>{dateTime.toLocaleString()}</span>
       </header>
       {/* <UseStateComponent /> */}
-      <UseEffectComponent/>
+      {/* <UseEffectComponent/> */}
     </>
   )
 }
